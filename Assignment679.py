b496b53f4a8ff5b8752726ffb7286b10d5a8f351
AnneStella <38238170+AnneStella@users.noreply.github.com>
2018-07-11 08:38:34 +0300

    Update Assignment679.py
    my number game editted

--- Assignment679.py
+++ Assignment679.py
@@ -1,40 +1,49 @@
 
-      import random
+import random
+
+digits = list(range(10))
+random.shuffle(digits)
+new_list = (digits[:3])
+print(new_list)
 
 print("BRAIN TEASERS!!\n Instructions \n Enter Y to continue and Q to quit the game")
 play = input("Do you want to play a game?")
 if play == "y":
-    print("Enter a three-digit number:")
-
-try:
-    digits = list(range(10))
-    random.shuffle(digits)
-    new_list = (digits[:3])
-    print(new_list)
-
-except ValueError:
-    print("Ooooopss!!!!This is not a number.Please try again.'")
+    print("welcome buckle up")
 
 
 def my_game():
-    y = 0
-    for x in range(0, 3):
-        if entry[x] in digits and entry[x] != digits[x]:
-            y = 1
-            print(f"Close {entry[x]}")
 
-    if y != 1:
-        c = 0
-        for x in range(0, 3):
-            if entry[x] == digits[x]:
-                c = 1
-                print(f"Match {entry[x]}")
+    empty_list = []
+    empty_string = ""
+    for x in entry:
+        if int(x) in new_list:
+            print(f"{int(x)} close")
+            empty_list.append(int(x))
+            empty_string += x
+        else:
+            pass
+    if len(empty_list) == 0:
+        print("nope")
 
-        if c != 1:
-            if entry != digits:
-                print("Nope")
+    else:
+        global c = 0
+        for i in empty_list:
+            pos_entry = entry.index(str(i))
+            pos_empty_list = empty_list.index(i)
+            if pos_entry == pos_empty_list:
+                print(f" {i} match")
+            c += 1
 
+            if c == 3:
+                print(f"bravo you guess the secret number {empty_string}")
 
 while True:
-    entry = input("Enter a 3-digit number:")
-    my_game()
+    try:
+        entry = input("Enter a 3-digit number:")
+        my_game()
+        if c == 3:
+            break
+
+    except ValueError:
+        print("only numbers")
